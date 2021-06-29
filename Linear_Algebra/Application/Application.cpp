652e8f96e8fc5a1a57f844134ecd2d8c3d1f3d70
Tom_Barten <17269387+TomBarten@users.noreply.github.com>
2021-06-29 14:36:44 +0200

    Removed SDL documentation, added basic drawing in SDL

--- Linear_Algebra/Application/Application.cpp
+++ Linear_Algebra/Application/Application.cpp
@@ -6,15 +6,62 @@
 
 auto main() -> int
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		std::cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
+
+		return 1;
 	}
-	else
+	
+	std::cout << "SDL initialization succeeded!";
+
+	SDL_Window* window = nullptr;
+
+	SDL_Renderer* renderer = nullptr;
+
+	if(SDL_CreateWindowAndRenderer(1000, 900, 0, &window, &renderer) != 0)
 	{
-		std::cout << "SDL initialization succeeded!";
+		std::cout << "Could not create window, SDL Error: " << SDL_GetError();
+
+		return 1;
 	}
 
+	SDL_bool done = SDL_FALSE;
+
+	while (!done)
+	{
+		SDL_Event event;
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderDrawLine(renderer, 320, 200, 300, 240);
+		SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
+		SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
+		SDL_RenderPresent(renderer);
+
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				done = SDL_TRUE;
+			}
+		}
+	}
+
+	if (renderer)
+	{
+		SDL_DestroyRenderer(renderer);
+	}
+
+	if (window)
+	{
+		SDL_DestroyWindow(window);
+	}
+
+	SDL_Quit();
+	
 	std::cin.get();
 
 	return 0;
