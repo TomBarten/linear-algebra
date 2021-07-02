a545a1ed7d2e85e61d9845fa6975c75b5155d5e0
Tom Barten <17269387+TomBarten@users.noreply.github.com>
2021-07-02 15:37:40 +0200

    added helper offset methods for drawing x y in center

--- Linear_Algebra/Application/main.cpp
+++ Linear_Algebra/Application/main.cpp
@@ -5,8 +5,37 @@
 #include <iostream>
 #include "vector2d.h"
 
-const auto WIDTH = 525.f;
-const auto HEIGHT = 525.f;
+const auto WIDTH = 500;
+const auto HEIGHT = 500;
+
+const auto x_origin = WIDTH / 2.f;
+const auto y_origin = HEIGHT / 2.f;
+
+inline auto draw_sdl_pixel(SDL_Renderer* renderer, float x, float y, const uint8_t r, const uint8_t g, const uint8_t b)
+{
+	x += x_origin;
+	y += y_origin;
+	y = HEIGHT - 1 - y;
+
+	SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+
+	SDL_RenderDrawPointF(renderer, x, y);
+}
+
+inline auto draw_sdl_line(SDL_Renderer* renderer, float x1, float y1, float x2, float y2, const uint8_t r, const uint8_t g, const uint8_t b)
+{
+	x1 += x_origin;
+	y1 += y_origin;
+	y1 = HEIGHT - 1 - y1;
+
+	x2 += x_origin;
+	y2 += y_origin;
+	y2 = HEIGHT - 1 - y2;
+
+	SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+
+	SDL_RenderDrawLineF(renderer, x1, y1, x2, y2);
+}
 
 auto main() -> int
 {
@@ -48,27 +77,57 @@ auto main() -> int
 		//SDL_RenderDrawLineF(renderer, 0, origin_y, WIDTH, origin_y);
 		//SDL_RenderDrawLineF(renderer, origin_x, 0, origin_x, HEIGHT);
 
-		const auto vector_x = std::make_unique<math::vector2d>(origin_x + 50, origin_y + 0);
+		//const auto vector_origin = std::make_unique<math::vector2d>(0,0);
 
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+		//draw_sdl_pixel(renderer, vector_origin->x(), vector_origin->y(), 255, 0, 0);
 
-		SDL_RenderDrawLineF(renderer, origin_x, origin_y, vector_x->x(), vector_x->y());
-		
+		const auto vector_x = std::make_unique<math::vector2d>(50, 0);
 
-		const auto vector_y = std::make_unique<math::vector2d>(origin_x + 0, origin_y + 50);
+		draw_sdl_line(renderer, 0,  0, vector_x->x(), vector_x->y(), 255, 0, 0);
 
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
-		
-		SDL_RenderDrawLineF(renderer, origin_x, origin_y, vector_y->x(), vector_y->y());
 
-		
-		const auto vector_xy_addition = (*vector_x) + (*vector_y);
+		const auto vector_y = std::make_unique<math::vector2d>(0, 50);
 
-		(*vector_xy_addition) *= 2.f;
+		draw_sdl_line(renderer, 0,  0, vector_y->x(), vector_y->y(), 0, 255, 0);
 
-		SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
+		auto vector_xy = (*vector_x) + (*vector_y);
 
-		SDL_RenderDrawLineF(renderer, origin_x, origin_y, vector_xy_addition->x(), vector_xy_addition->y());
+		draw_sdl_line(renderer, 0 , 0, vector_xy->x(), vector_xy->y(), 255, 255, 0);
+
+		(*vector_xy) *= -1;
+
+		draw_sdl_line(renderer, 0 , 0, vector_xy->x(), vector_xy->y(), 0, 0, 255);
+
+
+		vector_xy = (*vector_x) - (*vector_y);
+
+		draw_sdl_line(renderer, 0 , 0, vector_xy->x(), vector_xy->y(), 0, 255, 255);
+
+		(*vector_xy) *= -1;
+
+		draw_sdl_line(renderer, 0 , 0, vector_xy->x(), vector_xy->y(), 255, 0, 255);
+
+		//const auto vector_x = std::make_unique<math::vector2d>(origin_x + 50, origin_y + 0);
+
+		//SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+
+		//SDL_RenderDrawLineF(renderer, origin_x, origin_y, vector_x->x(), vector_x->y());
+		//
+
+		//const auto vector_y = std::make_unique<math::vector2d>(origin_x + 0, origin_y + 50);
+
+		//SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+		//
+		//SDL_RenderDrawLineF(renderer, origin_x, origin_y, vector_y->x(), vector_y->y());
+
+		//
+		//const auto vector_xy_addition = (*vector_x) + (*vector_y);
+
+		//(*vector_xy_addition) *= 2.f;
+
+		//SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
+
+		//SDL_RenderDrawLineF(renderer, origin_x, origin_y, vector_xy_addition->x(), vector_xy_addition->y());
 
 
 		SDL_RenderPresent(renderer);
